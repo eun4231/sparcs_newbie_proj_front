158574d463137c25f330687d49210883eac39e30
Ubuntu <ubuntu@ip-172-31-38-53.ap-northeast-2.compute.internal>
2020-11-22 20:43:36 +0000

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component {
+  render(){
+    return (
+      <header>
+        <h1>Campus Diary</h1>
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class TOC extends Component {
+  render(){
+    return (
+      <nav class="semester">
+        <ul>
+          <li><a href="1st spring">1st Spring</a></li>
+          <li><a href="1st summer">1st Summer</a></li>
+          <li><a href="1st autumn">1st Autumn</a></li>
+          <li><a href="1st winter">1st Winter</a></li>
+          <li><a href="2nd spring">2nd Spring</a></li>
+          <li><a href="2nd summer">2nd Summer</a></li>
+          <li><a href="2nd autumn">2nd Autumn</a></li>
+          <li><a href="2nd winter">2nd Winter</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Subject></Subject>
+      </div>
+    );
+  }
 }
 
 export default App;
